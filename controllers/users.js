25ed5f07c462ab30a18c8a37cfa7adc48ad9fdb0
Avisa-SEI <aap571420@gmail.com>
2021-06-12 16:31:00 -0700

    wip: add routes for home and user page

--- controllers/users.js
+++ controllers/users.js
@@ -1,9 +1,23 @@
 
 const User = require('../models/user')
 
-const router = require('express').Router()
+module.exports = {
+    index
+}
 
+function index(req, res) {
+    User.find({}, function(err, users) {
+        res.render('users/index', { 
+            users,
+            user: req.user
+         })
+    })
+}
 
+function addComment(req, res) {
+  
+}
 
+function delComment(req, res) {
 
-module.exports = router
+}
