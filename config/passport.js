cf4e7b0876815101915512f4bf32450a6b447f69
Avisa-SEI <aap571420@gmail.com>
2021-06-12 10:05:26 -0700

    wip: passport.js

--- config/passport.js
+++ config/passport.js
@@ -1,6 +1,7 @@
 
 const passport = require('passport')
 const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy
+const User = require('../models/user')
 
 // ^ passport.use <-- We use this to plug-in login option
 passport.use(new GoogleStrategy({
@@ -11,6 +12,27 @@ passport.use(new GoogleStrategy({
     // a user attempted a login
     // does this user already exist in our db?
     // if they don't we create then
+    User.findOne({ googleId: profile.id }, function(err, user) {
+        // * check for and handle errors
+        if (err) return cb(err)
+        // & if user exist in our db, logged them in
+        if (user) {
+            return cb(null, user)
+        } else {
+            //  ^ student doesn't exist, create the instead
+            const newUser = new User( { 
+                name: profile.displayName,
+                email: profile.emails[0].value,
+                googleId: profile.id
+            })
+
+            newUser.save(function(err) {
+                  if (err) return cb(err)
+                  // TODO user saved successfuly
+                  return cb(null, newUser)
+            })
+        }
+    })
 
 }))
 
