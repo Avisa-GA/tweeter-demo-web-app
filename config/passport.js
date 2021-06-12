c1101bd92f172c2a97c7e63093f4ddf7b6308684
Avisa-SEI <aap571420@gmail.com>
2021-06-12 10:27:02 -0700

    wip: done with serialize & deserialize

--- config/passport.js
+++ config/passport.js
@@ -8,11 +8,13 @@ passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_SECRET,
     callbackURL: process.env.GOOGLE_CALLBACK
-}, function(accessToken, refreshToken, profile, cb) {
+}, function (accessToken, refreshToken, profile, cb) {
     // a user attempted a login
     // does this user already exist in our db?
     // if they don't we create then
-    User.findOne({ googleId: profile.id }, function(err, user) {
+    User.findOne({
+        googleId: profile.id
+    }, function (err, user) {
         // * check for and handle errors
         if (err) return cb(err)
         // & if user exist in our db, logged them in
@@ -20,16 +22,16 @@ passport.use(new GoogleStrategy({
             return cb(null, user)
         } else {
             //  ^ student doesn't exist, create the instead
-            const newUser = new User( { 
+            const newUser = new User({
                 name: profile.displayName,
                 email: profile.emails[0].value,
                 googleId: profile.id
             })
 
-            newUser.save(function(err) {
-                  if (err) return cb(err)
-                  // TODO user saved successfuly
-                  return cb(null, newUser)
+            newUser.save(function (err) {
+                if (err) return cb(err)
+                // TODO user saved successfuly
+                return cb(null, newUser)
             })
         }
     })
@@ -37,7 +39,15 @@ passport.use(new GoogleStrategy({
 }))
 
 // & passport.serializeUser <-- gets called one time when the user logs in to create a session
+// * Session Storage & Cookie
+passport.serializeUser(function(user, done) {
+    done(null, user.id)
+})
 
 // ! passport.deserializeUser <-- get called with each request - then decodes the cookie and looks up the user in session store create req.user for us
 
-// ? 
+passport.deserializeUser(function(id, done) {
+    User.findById(id, function(err, user) {
+        done(err, user) // ? create req.user
+    })
+})
