49484d2cfc7b79c335b562568a5b48f06c5b0b70
Avisa-SEI <aap571420@gmail.com>
2021-06-14 07:23:33 -0700

    fix: call user on post route

--- controllers/home.js
+++ controllers/home.js
@@ -2,14 +2,27 @@
 const Tweet = require('../models/tweet')
 
 module.exports = {
-    index
+    index,
+    addTweet,
+    deleteTweet
 }
 
 function index(req, res) {
-    Tweet.find({ createdBy: req.params.id}, function(err, tweets) {
+    Tweet.find({}, function(err, tweets) {
         res.render('home/index', {
             tweets,
             user: req.user
         })
     })
+}
+
+function addTweet(req, res) {
+    req.body.createdBy = req.user._id
+    Tweet.create(req.body)
+    res.redirect('/home')
+}
+
+function deleteTweet(req, res) {
+      Tweet.findByIdAndDelete(req.params.id)
+      res.redirect('/home')
 }
