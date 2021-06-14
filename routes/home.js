d7173676d6100003dea7351106cf2eed91f39819
Avisa-SEI <aap571420@gmail.com>
2021-06-14 07:24:17 -0700

    wip:

--- routes/home.js
+++ routes/home.js
@@ -6,4 +6,10 @@ const homeCtrl = require('../controllers/home')
 // Get - home
 router.get('/home', homeCtrl.index)
 
+// Post / tweet
+router.post('/tweets', homeCtrl.addTweet)
+
+// Delete / tweets/:id
+router.delete('/tweets/:id', homeCtrl.deleteTweet)
+
 module.exports = router
