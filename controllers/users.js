523930697ad15ee4cf0ef4b18076688887b61bad
Avisa-SEI <aap571420@gmail.com>
2021-06-15 13:06:13 -0700

    wip: add func for routing

--- controllers/users.js
+++ controllers/users.js
@@ -1,19 +1,59 @@
-
 const User = require('../models/user')
 
 module.exports = {
-    index
+    index,
+    updateUser
 }
 
-function index(req, res) {
-    User.find({}, function(err, users) {
-        res.render('users/index', { 
+async function index(req, res) {
+    try {
+        const users = await User.find({})
+        res.render('users/index', {
             users,
             user: req.user
-         })
-    })
+        })
+    } catch (err) {
+        console.log(err)
+    }
 }
 
-function updateUser(req, res) {
-    
+async function updateUser(req, res) {
+    try {
+        await Tweet.findByIdAndUpdate(req.params.id, req.body, {
+            new: true
+        })
+        res.redirect(`/users`)
+    } catch (err) {
+        console.log(err)
+        res.redirect('/users')
+
+    }
 }
+
+// async function showUser(req, res) {
+//     try {
+
+//         const user = await User.findById(req.params.id)
+//         res.render('users/index', {
+//             user
+//         })
+
+//     } catch (err) {
+//         console.log(err)
+//         res.redirect('/users')
+//     }
+// }
+
+// async function editUser(req, res) {
+//     try {
+
+//         const user = await User.findById(req.params.id)
+//         res.render('users/index', {
+//             user
+//         })
+
+//     } catch (err) {
+//         console.log(err)
+//         res.render('/users')
+//     }
+// }
