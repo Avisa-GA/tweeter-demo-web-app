17c7fc29c6ff836592c68b6ab4f2222ff6d0eea3
Avisa-SEI <aap571420@gmail.com>
2021-06-16 10:41:20 -0700

    wip: Ajax news feed

--- public/js/script.js
+++ public/js/script.js
@@ -3,13 +3,18 @@
     const button = document.getElementById('edit-button')
     const editForm = document.getElementById('edit-form')
     const profileDetails = document.getElementById('profile-details')
+   
+   
 
     button.addEventListener('click', handleClick)
+   
+    
 
     function handleClick() {
         editForm.classList.toggle('hidden')
         profileDetails.classList.toggle('hidden')
     }
 
+   
 
 })();
