c1055039146745bb2ba7c74b4d7259d15407f713
Avisa-SEI <aap571420@gmail.com>
2021-06-16 11:39:14 -0700

    feat:

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,12 @@ const router = require('express').Router();
 const userCtrl = require('../controllers/users');
 
 // GET /users
-router.get('/users', userCtrl.index);
+router.get('/users', isLoggedIn, userCtrl.index);
+
+function isLoggedIn(req, res, next) {
+    if ( req.isAuthenticated() ) return next();
+    res.redirect('/auth/google'); // login or you can change this to another location
+}
 
 // Delete / users/:id
 router.delete('/users/:id', userCtrl.deleteUser)
